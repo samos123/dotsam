f9d5d1aa99b4c9125d42fa982a3f83d3ae83d844
Sam Stoelinga <sammiestoel@gmail.com>
2020-09-20 21:06:13 -0700

    Remove unneeded function from setup

--- setup.sh
+++ setup.sh
@@ -1,36 +1,5 @@
 #!/bin/bash
 
-function ask {
-    while true; do
- 
-        if [ "${2:-}" = "Y" ]; then
-            prompt="Y/n"
-            default=Y
-        elif [ "${2:-}" = "N" ]; then
-            prompt="y/N"
-            default=N
-        else
-            prompt="y/n"
-            default=
-        fi
- 
-        # Ask the question
-        read -p "$1 [$prompt] " REPLY
- 
-        # Default?
-        if [ -z "$REPLY" ]; then
-            REPLY=$default
-        fi
- 
-        # Check if the reply is valid
-        case "$REPLY" in
-            Y*|y*) return 0 ;;
-            N*|n*) return 1 ;;
-        esac
- 
-    done
-}
-
 # Get directory script is stored in
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -39,7 +8,7 @@ cp $DIR/zshrc ~/.zshrc
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Make zsh default shell (Requires root)
-sudo chsh -s /usr/bin/zsh
+sudo chsh -s /usr/bin/zsh $USER
 
 # Setup vim
 cp $DIR/vimrc ~/.vimrc
