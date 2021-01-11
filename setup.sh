5c26e664594acea213f23f79a0bceacca0c415e7
Sam Stoelinga <xxxemailxxx>
2021-01-10 20:48:00 -0800

    Update order

--- setup.sh
+++ setup.sh
@@ -3,12 +3,6 @@
 # Get directory script is stored in
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Setup oh-my-zsh and zsh
-cp $DIR/zshrc ~/.zshrc
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# Make zsh default shell (Requires root)
-sudo chsh -s /usr/bin/zsh $USER
 
 # Setup vim
 cp $DIR/vimrc ~/.vimrc
@@ -20,5 +14,13 @@ echo "Please enter your email address used for Git: "
 read email
 sed -i s/xxxemailxxx/$email/g ~/.gitconfig
 
-# Setup tmux and tmuxinator
+# Setup tmux
 cp $DIR/tmux.conf ~/.tmux.conf
+
+# Setup oh-my-zsh and zsh
+cp $DIR/zshrc ~/.zshrc
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Make zsh default shell (Requires root)
+sudo chsh -s /usr/bin/zsh $USER
+
